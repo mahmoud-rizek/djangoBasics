408ef6b23aebde91a43bfe023e2bd535ba29d699
Mahmoud Mohamed <mahmoudMohamed66@gmail.com>
2022-08-14 23:32:03 +0200

    upgrate

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,4 @@
+from distutils.command.upload import upload
 from django.db import models
 
 # Create your models here.
@@ -7,6 +8,7 @@ class postModel(models.Model):
     name = models.CharField(max_length=50)
     title = models.CharField(max_length=50)
     content = models.TextField(max_length=5000)
+    img = models.ImageField(upload_to='posts/')
     
     def __str__(self) -> str:
         return self.name
