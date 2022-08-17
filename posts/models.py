140083de30346462d92ce84440b5fcf19c22be0c
Mahmoud Mohamed <mahmoudMohamed66@gmail.com>
2022-08-17 19:20:12 +0200

    fix issue

--- posts/models.py
+++ posts/models.py
@@ -1,10 +1,12 @@
 from distutils.command.upload import upload
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 
 class postModel(models.Model):
+    auther = models.ForeignKey(User ,on_delete=models.CASCADE, related_name='user_post  ')
     name = models.CharField(max_length=50)
     title = models.CharField(max_length=50)
     content = models.TextField(max_length=5000)
